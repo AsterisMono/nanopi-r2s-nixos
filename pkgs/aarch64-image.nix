90ae821b5e4152a1bcce52fb19b2a23cb7650be3
Chatnoir Miki <cmiki@amono.me>
2024-08-17 21:19:45 +0800

    feat: prefetch aarch64 image sha

--- pkgs/aarch64-image.nix
+++ pkgs/aarch64-image.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
     # unfortunally there is no easy way right now to reproduce the same evaluation
     # as hydra, since `pkgs.path` is embedded in the binary
     url = "https://hydra.nixos.org/build/269682674/download/1/nixos-sd-image-24.05.3914.c3d4ac725177-aarch64-linux.img.zst";
-    sha256 = "0c7kqgp77433zq1710lpjwpmr06wvgx19767p9hlafa849lmq76q";
+    sha256 = "0g11hpsl8sw44vi7f6pmhxi1x59v61mj2vcpv5cz3j98r58w13y6";
   };
   preferLocalBuild = true;
   dontUnpack = true;
